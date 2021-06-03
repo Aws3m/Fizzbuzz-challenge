01ee7ecd75e91d595f1a1b8f7b871d029d48b276
Aws3m <semradijs@hotmail.com>
2021-06-03 16:26:33 +0200

    changes made:
    --> redone main program with better coding
    --> filled in the necessairy tests for the main program
    
    future plans:
    
    --> making code cleaner and search for more test possibilities to integrate

--- fizzbuzz tests.py
+++ fizzbuzz tests.py
@@ -1,11 +1,11 @@
 import unittest
 import fizzbuzz_challenge_main
 
-
-class fizzbuzzfizzbuzzChallenge(unittest.TestCase):
-    def test_(self):
-        self.assertEqual(True, False)
-
-
+class fizzbuzzChallenge(unittest.TestCase):
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(3), "Fizz", "expected Fizz, got something else")
+        self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(5), "Buzz", "expected Buzz, got something else")
+        self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(15), "FizzBuzz", "expected FizzBuzz, got something else")
+        self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(16), 16, "expected 16, got NaN")
 if __name__ == '__main__':
     unittest.main()
--- fizzbuzz_challenge_main.py
+++ fizzbuzz_challenge_main.py
@@ -4,19 +4,19 @@ But for multiples of three print Fizz instead of the number and for the multiple
 For numbers which are multiples of both three and five print FizzBuzz.
 Please also include any test you deem necessary."""
 
-def fizzbuzzChallenge():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+def fizzbuzz(index):
+    if (index % 3 == 0) and (index % 5 == 0):
+        return "FizzBuzz"
+    elif index % 3 == 0:
+        return "Fizz"
+    elif index % 5 == 0:
+        return "Buzz"
+    else:
+        return index
 
 def main():
-    fizzbuzzChallenge()
+    for i in range(1, 101):
+        print(fizzbuzz(i))
 
 if __name__ == '__main__':
     main()
