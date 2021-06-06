8f5c6c6ebadf65de9e908bb196a513ab58fdcb78
Aws3m <semradijs@hotmail.com>
2021-06-06 19:56:14 +0200

    last changes:
    - removed unnecessairy comments in code
    - added error message to tests

--- fizzbuzz tests.py
+++ fizzbuzz tests.py
@@ -5,23 +5,19 @@ class fizzbuzzChallenge(unittest.TestCase):
 
     def test_multiplesOfThree(self):
         for i in [3, 6, 9, 36]:
-            #self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(i), "Fizz", "expected Fizz, got something else")
-            assert fizzbuzz_challenge_main.fizzbuzz(i) == "Fizz"
+            assert fizzbuzz_challenge_main.fizzbuzz(i) == "Fizz", "expected Fizz"
 
     def test_multiplesOfFive(self):
         for i in [5, 10, 50, 95]:
-            #self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(i), "Buzz", "expected Buzz, got something else")
-            assert fizzbuzz_challenge_main.fizzbuzz(i) == "Buzz"
+            assert fizzbuzz_challenge_main.fizzbuzz(i) == "Buzz", "expected Buzz"
 
     def test_MultiplesOfThreeAndFive(self):
         for i in [15, 30, 45, 75]:
-            #self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(i), "FizzBuzz", "expected FizzBuzz, got something else")
-            assert fizzbuzz_challenge_main.fizzbuzz(i) == "FizzBuzz"
+            assert fizzbuzz_challenge_main.fizzbuzz(i) == "FizzBuzz", "expected FizzBuzz"
 
     def test_regularNumbers(self):
         for i in [2, 4, 41, 88]:
-            #self.assertEqual(fizzbuzz_challenge_main.fizzbuzz(i), i, "expected numbers, got NaN")
-            assert fizzbuzz_challenge_main.fizzbuzz(i) == i
+            assert fizzbuzz_challenge_main.fizzbuzz(i) == i, "expected number"
 
 if __name__ == '__main__':
     unittest.main()
